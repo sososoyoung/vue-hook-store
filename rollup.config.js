7415c4b45758ffeaf7212b128b3b5d0b8687e177
Jason <shty1990@163.com>
2021-05-08 15:42:21 +0800

    fix: 修复低版本浏览器不兼容问题

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+// rollup.config.js
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { join } from "path";
+import PKG_JSON from "./package.json";
+
+const external = [...new Set([...Object.keys(PKG_JSON.peerDependencies || {})])];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.esm.js",
+        format: "es",
+        name: "vue-hook-store",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        name: "vue-hook-store",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.umd.js",
+        format: "umd",
+        name: "vueHookStore",
+        sourcemap: true,
+      },
+    ],
+    external,
+    plugins: [
+      resolve({ browser: true }),
+      commonjs(),
+      typescript({
+        clean: true,
+        tsconfig: "tsconfig.json",
+        rollupCommonJSResolveHack: false,
+      }),
+    ],
+  },
+];
